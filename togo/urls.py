1b9c5a000dcd9a949575fac784088c9dc2676b0d
monarh45yt-web <monarh45yt@gmail.com>
2026-02-12 03:33:00 +0600

    added admin panel

--- togo/urls.py
+++ togo/urls.py
@@ -25,4 +25,4 @@ urlpatterns = [
     path('', homepage, name="home"),
     path('test/', test, name="test"),
 ]   + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
-+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
