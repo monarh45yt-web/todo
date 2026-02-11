72fe2fa91ad768169bbb56a11f642c3fa344fa54
monarh45yt-web <monarh45yt@gmail.com>
2026-02-12 04:17:04 +0600

    added oop.html

--- main/views.py
+++ main/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from .models import ToDo
 
 def homepage(request):
     return render(request, "oop.html")
 
 def test(request):
-    return render(request, "test.html")
+    todo_list = ToDo.objects.all()
+    return render(request, "test.html", {"todo_list": todo_list})
