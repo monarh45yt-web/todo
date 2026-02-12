b9589177c5b3c733950fa213f9e8a022463ce0d2
monarh45yt-web <monarh45yt@gmail.com>
2026-02-12 06:16:26 +0600

    kones

--- togo/urls.py
+++ togo/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from main.views import homepage, test, add_todo
+from main.views import homepage, test, add_todo, delete_todo, mark_todo, close_todo
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -25,5 +25,8 @@ urlpatterns = [
     path('', homepage, name="home"),
     path('test/', test, name="test"),
     path("add-todo", add_todo, name="add-todo"),
+    path("delete-todo/<id>/", delete_todo, name="delete-todo"),
+    path("mark-todo/<id>/", mark_todo, name="mark-todo"),
+    path("close-todo/<id>/", close_todo, name="close-todo"),
 ]   + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
     + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
